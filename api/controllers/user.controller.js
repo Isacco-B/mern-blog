d3bcfe1cf0c6f7590478e5ac7fc60afeffbbd212
Isacco-B <git@isaccobertoli.com>
2024-01-31 14:06:11 +0100

    add delete user api router

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "API is working" });
@@ -47,9 +47,21 @@ export const updateUser = async (req, res, next) => {
       },
       { new: true }
     );
-    const {password, ...rest} = updateUser._doc;
-    res.status(200).json(rest)
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findOneAndDelete(req.params.userId)
+    res.status(200).json("User has benn deleted")
+  } catch (error) {
+    next(error)
   }
 };
