7188c5a6308f05ca4405fa6ba83d3599951cacdb
Isacco-B <git@isaccobertoli.com>
2024-02-01 16:06:29 +0100

    create getUser api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,11 +5,13 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUser,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.get("/getusers", verifyToken, getUser);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
